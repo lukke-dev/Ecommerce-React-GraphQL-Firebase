c214526c88b00b02cc72c2b2c1943a6dac6c577c
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-01-09 17:12:50 -0300

    Finished Sign in Sign up page

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import FormInput from "../FormInput";
 import CustomButton from "../CustomButton";
 import "./style.scss";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -14,9 +14,18 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({ email: "", password: "" });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   handleChange = (event) => {
